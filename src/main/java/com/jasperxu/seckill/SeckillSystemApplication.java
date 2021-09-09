97ade3898302460f3e805df4129820d65cb4de4f
Jasper Xu <xzh1401759561@gmail.com>
2021-09-08 23:56:42 -0400

    creat the project, initial commit

---
+++ src/main/java/com/jasperxu/seckill/SeckillSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jasperxu.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasperxu/seckill/SeckillSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasperxu.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
