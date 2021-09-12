658361f1d9bf0a39112554cc2d9273d59be7f00d
Jasper Xu <xzh1401759561@gmail.com>
2021-09-12 18:09:05 -0400

    Added annotation for scanning

--- src/main/java/com/jasperxu/seckill/SeckillSystemApplication.java
+++ src/main/java/com/jasperxu/seckill/SeckillSystemApplication.java
@@ -1,9 +1,13 @@
 package com.jasperxu.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.jasperxu.seckill.db.mappers")
+@ComponentScan(basePackages = {"com.jasperxu"})
 public class SeckillSystemApplication {
 
     public static void main(String[] args) {
