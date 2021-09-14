47d72969e53bc6b039b983e0ab11d2a1568e8484
Jasper Xu <xzh1401759561@gmail.com>
2021-09-13 22:29:32 -0400

    Added test for MyBatis integration and DAO

---
+++ src/test/java/com/jasperxu/seckill/DaoTest.java
@@ -0,0 +1,43 @@
+package com.jasperxu.seckill;
+
+import com.jasperxu.seckill.db.dao.SeckillActivityDao;
+import com.jasperxu.seckill.db.mappers.SeckillActivityMapper;
+import com.jasperxu.seckill.db.po.SeckillActivity;
+
+import java.math.BigDecimal;
+import java.util.List;
+import javax.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DaoTest {
+    @Resource
+    private SeckillActivityMapper seckillActivityMapper;
+
+    @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
+    @Test
+    void SeckillActivityTest() {
+        SeckillActivity seckillActivity = new SeckillActivity();
+        seckillActivity.setName("Activity 1");
+        seckillActivity.setCommodityId(999L);
+        seckillActivity.setTotalStock(100L);
+        seckillActivity.setSeckillPrice(new BigDecimal(99));
+        seckillActivity.setActivityStatus(1);
+        seckillActivity.setOldPrice(new BigDecimal(199));
+        seckillActivity.setAvailableStock(100);
+        seckillActivity.setLockStock(0L);
+        seckillActivityMapper.insert(seckillActivity);
+        System.out.println("====>>>>" + seckillActivityMapper.selectByPrimaryKey(1L));
+    }
+
+    @Test
+    void setSeckillActivityQuery() {
+        List<SeckillActivity> seckillActivities = seckillActivityDao.querySeckillActivitiesByStatus(0);
+        System.out.println(seckillActivities.size());
+        seckillActivities.stream().forEach(seckillActivity -> System.out.println(seckillActivity.toString()));
+    }
+}
