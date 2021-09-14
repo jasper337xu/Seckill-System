7d61c74e8f156fbe49b2674cbf84b6c2618a0540
Jasper Xu <xzh1401759561@gmail.com>
2021-09-13 22:18:43 -0400

    Added a new query method

--- src/main/java/com/jasperxu/seckill/db/mappers/SeckillActivityMapper.java
+++ src/main/java/com/jasperxu/seckill/db/mappers/SeckillActivityMapper.java
@@ -2,6 +2,8 @@ package com.jasperxu.seckill.db.mappers;
 
 import com.jasperxu.seckill.db.po.SeckillActivity;
 
+import java.util.List;
+
 public interface SeckillActivityMapper {
     int deleteByPrimaryKey(Long id);
 
@@ -14,4 +16,6 @@ public interface SeckillActivityMapper {
     int updateByPrimaryKeySelective(SeckillActivity record);
 
     int updateByPrimaryKey(SeckillActivity record);
+
+    List<SeckillActivity> querySeckillActivitiesByStatus(int activityStatus);
 }
