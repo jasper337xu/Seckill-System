1cac9700c1a562a9fb8cefcae5416ccd1886d0f0
Jasper Xu <xzh1401759561@gmail.com>
2021-09-16 00:01:33 -0400

    Added a new method that creates a new seckill promotion activity and stores it to db

--- src/main/java/com/jasperxu/seckill/db/controller/SeckillActivityController.java
+++ src/main/java/com/jasperxu/seckill/db/controller/SeckillActivityController.java
@@ -1,12 +1,54 @@
 package com.jasperxu.seckill.db.controller;
 
+import com.jasperxu.seckill.db.dao.SeckillActivityDao;
+import com.jasperxu.seckill.db.po.SeckillActivity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Controller
 public class SeckillActivityController {
+
+    @Autowired
+    private SeckillActivityDao seckillActivityDao;
+
     @RequestMapping("/addSeckillActivity")
     public String addSeckillActivity() {
         return "add_activity";
+    }
+
+    @ResponseBody
+    @RequestMapping("/addSeckillActivityAction")
+    public String addSeckillActivityAction(
+            @RequestParam("name") String name,
+            @RequestParam("commodityId") long commodityId,
+            @RequestParam("seckillPrice") BigDecimal seckillPrice,
+            @RequestParam("oldPrice") BigDecimal oldPrice,
+            @RequestParam("seckillNumber") long seckillNumber,
+            @RequestParam("startTime") String startTime,
+            @RequestParam("endTime") String endTime
+    ) throws ParseException {
+        startTime = startTime.substring(0, 10) + startTime.substring(11);
+        endTime = endTime.substring(0, 10) + endTime.substring(11);
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-ddhh:mm");
+        SeckillActivity seckillActivity = new SeckillActivity();
+        seckillActivity.setName(name);
+        seckillActivity.setCommodityId(commodityId);
+        seckillActivity.setSeckillPrice(seckillPrice);
+        seckillActivity.setOldPrice(oldPrice);
+        seckillActivity.setTotalStock(seckillNumber);
+        seckillActivity.setAvailableStock(new Integer("" + seckillNumber));
+        seckillActivity.setLockStock(0L);
+        seckillActivity.setActivityStatus(1);
+        seckillActivity.setStartTime(format.parse(startTime));
+        seckillActivity.setEndTime(format.parse(endTime));
+        seckillActivityDao.insertSeckillActivity(seckillActivity);
+        return seckillActivity.toString();
     }
 }
