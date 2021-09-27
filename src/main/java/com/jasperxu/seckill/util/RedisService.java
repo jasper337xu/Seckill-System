38fd8952cc3fdb0b53ebc52701051d594c791319
Jasper Xu <xzh1401759561@gmail.com>
2021-09-27 02:13:06 -0400

    Added stock deduction validation using Lua script

--- src/main/java/com/jasperxu/seckill/util/RedisService.java
+++ src/main/java/com/jasperxu/seckill/util/RedisService.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @Service
 public class RedisService {
@@ -22,5 +23,31 @@ public class RedisService {
         String value = jedisClient.get(key);
         jedisClient.close();
         return value;
+    }
+
+    public boolean validateStockDeduction(String key) {
+        try(Jedis jedisClient = jedisPool.getResource()) {
+            String script =
+                    "if redis.call('exists',KEYS[1]) == 1 then\n" +
+                    "    local stock = tonumber(redis.call('get', KEYS[1]))\n" +
+                    "    if (stock <= 0) then\n" +
+                    "       return -1\n" +
+                    "    end;\n" +
+                    "    redis.call('decr',KEYS[1]);\n" +
+                    "    return stock - 1;\n" +
+                    "end;\n" +
+                    "return -1;";
+            Long stock = (Long) jedisClient.eval(script, Collections.singletonList(key), Collections.emptyList());
+
+            if (stock < 0) {
+                System.out.println("Stock Unavailable");
+                return false;
+            }
+            System.out.println("Stock successfully deducted");
+            return true;
+        } catch (Throwable throwable) {
+            System.out.println("Stock deduction failed：" + throwable.toString());
+            return false;
+        }
     }
 }
