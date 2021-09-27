56663fbda7668f03f8781aabcef6ae74c8612e69
Jasper Xu <xzh1401759561@gmail.com>
2021-09-26 23:04:43 -0400

    Created a service for Redis that has set and get methods

---
+++ src/main/java/com/jasperxu/seckill/util/RedisService.java
@@ -0,0 +1,26 @@
+package com.jasperxu.seckill.util;
+
+import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.annotation.Resource;
+
+@Service
+public class RedisService {
+    @Resource
+    private JedisPool jedisPool;
+
+    public void setValue(String key, Long value) {
+        Jedis jedisClient = jedisPool.getResource();
+        jedisClient.set(key, value.toString());
+        jedisClient.close();
+    }
+
+    public String getValue(String key) {
+        Jedis jedisClient = jedisPool.getResource();
+        String value = jedisClient.get(key);
+        jedisClient.close();
+        return value;
+    }
+}
