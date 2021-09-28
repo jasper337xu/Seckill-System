7a7bd64140f2b416fdbd45311b2ddbfc1b511182
Jasper Xu <xzh1401759561@gmail.com>
2021-09-27 22:36:51 -0400

    Updated processSeckillOrder method that uses Redis with Lua script so that a large number of concurrent requests can be handled

--- src/main/java/com/jasperxu/seckill/controller/SeckillOversellController.java
+++ src/main/java/com/jasperxu/seckill/controller/SeckillOversellController.java
@@ -1,20 +1,38 @@
 package com.jasperxu.seckill.controller;
 
-import com.jasperxu.seckill.service.SeckillOversellService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jasperxu.seckill.service.SeckillActivityService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+
 @Controller
 public class SeckillOversellController {
-    @Autowired
-    private SeckillOversellService seckillOversellService;
+//    @Resource
+//    private SeckillOversellService seckillOversellService;
+
+    @Resource
+    private SeckillActivityService seckillActivityService;
+
+//    /*
+//    Uses the simple version method to process order,
+//    NOT able to handle a large number of concurrent requests
+//    */
+//    @ResponseBody
+//    @RequestMapping("/seckill/commodity/{seckillActivityId}")
+//    public String processSeckillOrder(@PathVariable long seckillActivityId){
+//        return seckillOversellService.processSeckillOrder(seckillActivityId);
+//    }
 
     @ResponseBody
-    @RequestMapping("/seckill/order/{seckillActivityId}")
+    @RequestMapping("/seckill/commodity/{seckillActivityId}")
     public String processSeckillOrder(@PathVariable long seckillActivityId){
-        return seckillOversellService.processSeckillOrder(seckillActivityId);
+        boolean stockValidationResult = seckillActivityService.validateAndProcessSeckillOrder(seckillActivityId);
+        if (stockValidationResult) {
+            return "Thanks for shopping! Your order has been processed!";
+        }
+        return "Sorry! The item you ordered has been sold out.";
     }
 }
