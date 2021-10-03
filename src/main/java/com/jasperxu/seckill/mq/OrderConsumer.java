e7115189ca4dd47cbbc791883b7c165748d99025
Jasper Xu <xzh1401759561@gmail.com>
2021-10-02 23:44:13 -0400

    Created OrderConsumer that retrieves ID, locks stock and inserts into database

---
+++ src/main/java/com/jasperxu/seckill/mq/OrderConsumer.java
@@ -0,0 +1,48 @@
+package com.jasperxu.seckill.mq;
+
+import com.alibaba.fastjson.JSON;
+import com.jasperxu.seckill.db.dao.OrderDao;
+import com.jasperxu.seckill.db.dao.SeckillActivityDao;
+import com.jasperxu.seckill.db.po.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RocketMQMessageListener(topic = "order", consumerGroup = "order_group")
+public class OrderConsumer implements RocketMQListener<MessageExt> {
+    @Resource
+    private OrderDao orderDao;
+
+    @Resource
+    private SeckillActivityDao seckillActivityDao;
+
+    public void onMessage (MessageExt messageExt) {
+        // 1. Parse messages into orders
+        String message = new String(messageExt.getBody(), StandardCharsets.UTF_8);
+        log.info("Received request for creating an order：" + message);
+        Order order = JSON.parseObject(message, Order.class);
+        order.setCreateTime(new Date());
+
+        // 2. Lock stock
+        boolean lockStockResult = seckillActivityDao.lockStock(order.getSeckillActivityId());
+        if (lockStockResult) {
+            // Order Status 1: Order created, waiting for checkout
+            order.setOrderStatus(1);
+        } else {
+            // Order Status 0: No available stock，invalid order
+            order.setOrderStatus(0);
+        }
+
+        // 3. Insert a new order(record) to database
+        orderDao.insertOrder(order);
+    }
+
+}
