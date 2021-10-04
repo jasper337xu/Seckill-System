727aa386119c6e7bbe9f284c4ef3106d69f752ef
Jasper Xu <xzh1401759561@gmail.com>
2021-10-03 23:24:15 -0400

    Added a method that processes order request

--- src/main/java/com/jasperxu/seckill/controller/SeckillActivityController.java
+++ src/main/java/com/jasperxu/seckill/controller/SeckillActivityController.java
@@ -2,28 +2,36 @@ package com.jasperxu.seckill.controller;
 
 import com.jasperxu.seckill.db.dao.SeckillActivityDao;
 import com.jasperxu.seckill.db.dao.SeckillCommodityDao;
+import com.jasperxu.seckill.db.po.Order;
 import com.jasperxu.seckill.db.po.SeckillActivity;
 import com.jasperxu.seckill.db.po.SeckillCommodity;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.jasperxu.seckill.service.SeckillActivityService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Controller
 public class SeckillActivityController {
 
-    @Autowired
+    @Resource
     private SeckillActivityDao seckillActivityDao;
 
-    @Autowired
+    @Resource
     private SeckillCommodityDao seckillCommodityDao;
+
+    @Resource
+    SeckillActivityService seckillActivityService;
 
     @RequestMapping("/seckillActivities")
     public String getAllOngoingSeckillActivities(Map<String, Object> resultMap) {
@@ -88,5 +96,34 @@ public class SeckillActivityController {
         resultMap.put("seckillActivity", seckillActivity);
 
         return "add_success";
+    }
+
+    /**
+     * Process seckill request
+     * @param userId
+     * @param seckillActivityId
+     * @return
+     */
+    @RequestMapping("/seckill/order/{userId}/{seckillActivityId}")
+    public ModelAndView seckillCommodity(@PathVariable long userId, @PathVariable long seckillActivityId) {
+        boolean stockValidationResult = false;
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            stockValidationResult = seckillActivityService.validateStock(seckillActivityId);
+            if (stockValidationResult) {
+                Order order = seckillActivityService.createOrder(seckillActivityId, userId);
+                modelAndView.addObject("resultInfo","Seckill succeeds! " +
+                        "Creating the order，order ID：" + order.getOrderNo());
+                modelAndView.addObject("orderNo", order.getOrderNo());
+            }
+            else {
+                modelAndView.addObject("resultInfo","Sorry，there is no available stock.");
+            }
+        } catch (Exception e) {
+            log.error("Seckill System throws exception: " + e.toString());
+            modelAndView.addObject("resultInfo","Seckill fails.");
+        }
+        modelAndView.setViewName("seckill_result");
+        return modelAndView;
     }
 }
