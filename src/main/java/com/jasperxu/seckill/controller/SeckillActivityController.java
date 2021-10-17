64db66b652f202eb43dc0535617ee02a5af12fab
Jasper Xu <xzh1401759561@gmail.com>
2021-10-16 23:35:26 -0400

    Added flow control implementation to guarantee high reliability and stability of the system

--- src/main/java/com/jasperxu/seckill/controller/SeckillActivityController.java
+++ src/main/java/com/jasperxu/seckill/controller/SeckillActivityController.java
@@ -1,5 +1,11 @@
 package com.jasperxu.seckill.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.alibaba.fastjson.JSON;
 import com.jasperxu.seckill.db.dao.OrderDao;
 import com.jasperxu.seckill.db.dao.SeckillActivityDao;
@@ -16,10 +22,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -42,49 +50,148 @@ public class SeckillActivityController {
     @Resource
     RedisService redisService;
 
-    @RequestMapping("/seckillActivities")
-    public String getAllOngoingSeckillActivities(Map<String, Object> resultMap) {
-        // query all ongoing seckill activities
-        List<SeckillActivity> seckillActivities = seckillActivityDao.querySeckillActivitiesByStatus(1);
-        resultMap.put("seckillActivities", seckillActivities);
-        return "seckill_activity";
+    @PostConstruct
+    public void defineFlowControlRules() {
+        List<FlowRule> flowRules = new ArrayList<>();
+
+        FlowRule flowRule = new FlowRule();
+        // define the resource to which this flow rule applies
+        flowRule.setResource("SeckillActivities");
+        // define the type of flow control rule to be QPS
+        flowRule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // define the max number of requests per second the resource can respond to
+        flowRule.setCount(100000);
+
+        FlowRule flowRule2 = new FlowRule();
+        // define the resource to which this flow rule applies
+        flowRule2.setResource("CommodityDetails");
+        // define the type of flow control rule to be QPS
+        flowRule2.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // define the max number of requests per second the resource can respond to
+        flowRule2.setCount(2);
+
+        flowRules.add(flowRule);
+        flowRules.add(flowRule2);
+        FlowRuleManager.loadRules(flowRules);
     }
 
-    @RequestMapping("/item/{seckillActivityId}")
-    public String itemPage(Map<String,Object> resultMap,@PathVariable long seckillActivityId) {
+    @RequestMapping("/seckill/activities")
+    public String retrieveAllOngoingSeckillActivities(Map<String, Object> resultMap) {
+        try (Entry entry = SphU.entry("SeckillActivities")) {
+            // query all ongoing seckill activities
+            List<SeckillActivity> seckillActivities = seckillActivityDao.querySeckillActivitiesByStatus(1);
+            resultMap.put("seckillActivities", seckillActivities);
+            return "seckill_activity";
+        } catch (BlockException e) {
+            log.error("Sentinel controls the flow to resource SeckillActivities since the maximum number of requests per second is exceeded. " + e.toString());
+            return "wait";
+        }
+    }
+
+    @RequestMapping("/seckill/commodity/detail/{seckillActivityId}")
+    public String retrieveCommodityDetails(Map<String,Object> resultMap, @PathVariable long seckillActivityId) {
         SeckillActivity seckillActivity;
         SeckillCommodity seckillCommodity;
 
-        String activityInfo = redisService.getValue("seckillActivity:" + seckillActivityId);
-        if (activityInfo == null || activityInfo.equals("")) {
-            seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        try (Entry entry = SphU.entry("CommodityDetails")) {
+            String activityInfo = redisService.getValue("seckillActivity:" + seckillActivityId);
+            if (activityInfo == null || activityInfo.equals("")) {
+                seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+            } else {
+                // Seckill activity has been stored into Redis Cache.
+                // Retrieving from cache is much faster than retrieving from database.
+                log.info("Seckill activity info retrieved from Redis Cache: " + activityInfo);
+                seckillActivity = JSON.parseObject(activityInfo, SeckillActivity.class);
+            }
+
+            String commodityInfo = redisService.getValue("seckillCommodity:" + seckillActivity.getCommodityId());
+            if (commodityInfo == null || commodityInfo.equals("")) {
+                seckillCommodity = seckillCommodityDao.querySeckillCommodityById(seckillActivity.getCommodityId());
+            } else {
+                // Seckill commodity has been stored into Redis Cache.
+                // Retrieving from cache is much faster than retrieving from database.
+                log.info("Seckill commodity info retrieved from Redis Cache: " + commodityInfo);
+                seckillCommodity = JSON.parseObject(commodityInfo, SeckillCommodity.class);
+            }
+
+            resultMap.put("seckillActivity", seckillActivity);
+            resultMap.put("seckillCommodity", seckillCommodity);
+            resultMap.put("seckillPrice", seckillActivity.getSeckillPrice());
+            resultMap.put("oldPrice", seckillActivity.getOldPrice());
+            resultMap.put("commodityId", seckillActivity.getCommodityId());
+            resultMap.put("commodityName", seckillCommodity.getCommodityName());
+            resultMap.put("commodityDesc", seckillCommodity.getCommodityDesc());
+            return "seckill_item";
+        }
+        catch (BlockException e) {
+            log.error("Sentinel controls the flow to resource CommodityDetails since the maximum number of requests per second is exceeded. " + e.toString());
+            return "wait";
+        }
+    }
+
+    /**
+     * Retrieve order info
+     * @param orderNo
+     * @return
+     */
+    @RequestMapping("/seckill/orderInfo/{orderNo}")
+    public ModelAndView retrieveOrderInfo(@PathVariable String orderNo) {
+        Order order = orderDao.queryOrder(orderNo);
+        ModelAndView modelAndView = new ModelAndView();
+
+        if (order != null) {
+            modelAndView.setViewName("order_info");
+            modelAndView.addObject("order", order);
+            SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(order.getSeckillActivityId());
+            modelAndView.addObject("seckillActivity", seckillActivity);
         }
         else {
-            // Seckill activity has been stored into Redis Cache.
-            // Retrieving from cache is much faster than retrieving from database.
-            log.info("Seckill activity info retrieved from Redis Cache: " + activityInfo);
-            seckillActivity = JSON.parseObject(activityInfo, SeckillActivity.class);
+            modelAndView.setViewName("order_wait");
         }
 
-        String commodityInfo = redisService.getValue("seckillCommodity:" + seckillActivity.getCommodityId());
-        if (commodityInfo == null || commodityInfo.equals("")) {
-            seckillCommodity = seckillCommodityDao.querySeckillCommodityById(seckillActivity.getCommodityId());
-        }
-        else {
-            // Seckill commodity has been stored into Redis Cache.
-            // Retrieving from cache is much faster than retrieving from database.
-            log.info("Seckill commodity info retrieved from Redis Cache: " + commodityInfo);
-            seckillCommodity = JSON.parseObject(commodityInfo, SeckillCommodity.class);
+        return modelAndView;
+    }
+
+    /**
+     * Process seckill request (order creation)
+     * @param userId
+     * @param seckillActivityId
+     * @return
+     */
+    @RequestMapping("/seckill/purchase/{userId}/{seckillActivityId}")
+    public ModelAndView processOrderCreation(@PathVariable long userId, @PathVariable long seckillActivityId) {
+        boolean stockValidationResult = false;
+        ModelAndView modelAndView = new ModelAndView();
+
+        try {
+            stockValidationResult = seckillActivityService.validateStock(seckillActivityId);
+            if (stockValidationResult) {
+                Order order = seckillActivityService.createOrder(seckillActivityId, userId);
+                modelAndView.addObject("resultInfo","Seckill succeeds! " +
+                        "Creating the order，order ID：" + order.getOrderNo());
+                modelAndView.addObject("orderNo", order.getOrderNo());
+            }
+            else {
+                modelAndView.addObject("resultInfo","Sorry，there is no available stock.");
+            }
+        } catch (Exception e) {
+            log.error("Seckill System throws exception: " + e.toString());
+            modelAndView.addObject("resultInfo","Seckill fails.");
         }
 
-        resultMap.put("seckillActivity", seckillActivity);
-        resultMap.put("seckillCommodity", seckillCommodity);
-        resultMap.put("seckillPrice", seckillActivity.getSeckillPrice());
-        resultMap.put("oldPrice", seckillActivity.getOldPrice());
-        resultMap.put("commodityId", seckillActivity.getCommodityId());
-        resultMap.put("commodityName", seckillCommodity.getCommodityName());
-        resultMap.put("commodityDesc", seckillCommodity.getCommodityDesc());
-        return "seckill_item";
+        modelAndView.setViewName("seckill_result");
+        return modelAndView;
+    }
+
+    /**
+     * Process order checkout
+     * @param orderNo
+     * @return
+     */
+    @RequestMapping("/seckill/checkout/{orderNo}")
+    public String processOrderCheckout(@PathVariable String orderNo) throws Exception {
+        seckillActivityService.checkoutProcess(orderNo);
+        return "redirect:/seckill/orderInfo/" + orderNo;
     }
 
     @RequestMapping("/addSeckillActivity")
@@ -127,69 +234,5 @@ public class SeckillActivityController {
         resultMap.put("seckillActivity", seckillActivity);
 
         return "add_success";
-    }
-
-    /**
-     * Process seckill request
-     * @param userId
-     * @param seckillActivityId
-     * @return
-     */
-    @RequestMapping("/seckill/purchase/{userId}/{seckillActivityId}")
-    public ModelAndView seckillCommodity(@PathVariable long userId, @PathVariable long seckillActivityId) {
-        boolean stockValidationResult = false;
-        ModelAndView modelAndView = new ModelAndView();
-
-        try {
-            stockValidationResult = seckillActivityService.validateStock(seckillActivityId);
-            if (stockValidationResult) {
-                Order order = seckillActivityService.createOrder(seckillActivityId, userId);
-                modelAndView.addObject("resultInfo","Seckill succeeds! " +
-                        "Creating the order，order ID：" + order.getOrderNo());
-                modelAndView.addObject("orderNo", order.getOrderNo());
-            }
-            else {
-                modelAndView.addObject("resultInfo","Sorry，there is no available stock.");
-            }
-        } catch (Exception e) {
-            log.error("Seckill System throws exception: " + e.toString());
-            modelAndView.addObject("resultInfo","Seckill fails.");
-        }
-
-        modelAndView.setViewName("seckill_result");
-        return modelAndView;
-    }
-
-    /**
-     * Get order info
-     * @param orderNo
-     * @return
-     */
-    @RequestMapping("/seckill/orderInfo/{orderNo}")
-    public ModelAndView orderInfo(@PathVariable String orderNo) {
-        Order order = orderDao.queryOrder(orderNo);
-        ModelAndView modelAndView = new ModelAndView();
-
-        if (order != null) {
-            modelAndView.setViewName("order_info");
-            modelAndView.addObject("order", order);
-            SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(order.getSeckillActivityId());
-            modelAndView.addObject("seckillActivity", seckillActivity);
-        }
-        else {
-            modelAndView.setViewName("order_wait");
-        }
-
-        return modelAndView;
-    }
-
-    /**
-     * Order checkout
-     * @return
-     */
-    @RequestMapping("/seckill/checkout/{orderNo}")
-    public String orderCheckout(@PathVariable String orderNo) throws Exception {
-        seckillActivityService.checkoutProcess(orderNo);
-        return "redirect:/seckill/orderInfo/" + orderNo;
     }
 }
